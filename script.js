fbe0d763f99c63ea06fe628c9fd1e9e136fba5a8
chonjessica23 <chon.jessica@gmail.com>
2018-10-18 01:45:40 -0400

    commit message

---
+++ script.js
@@ -0,0 +1,12 @@
+function onPenClick() {
+	document.getElementById("schooltext").style.display = "block"
+	document.getElementById("ima").style.display = "block"
+	document.getElementById("nyush").style.display = "block"
+}
+
+
+function onPersonClick() {
+	document.getElementById("bubble").style.display = "block"
+	document.getElementById("bubbledot1").style.display = "block"
+	document.getElementById("bubbledot2").style.display = "block"
+}
